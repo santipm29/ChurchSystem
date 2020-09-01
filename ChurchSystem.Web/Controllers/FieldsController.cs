2197de592e8cc531d9a4623a4ae0702d52cf9aa7
Santiago Patiño <santipmartinez@gmail.com>
2020-09-01 17:32:38 -0500

    added fieldcontroller

---
+++ ChurchSystem.Web/Controllers/FieldsController.cs
@@ -0,0 +1,156 @@
+﻿using ChurchSystem.Common.Entities;
+using ChurchSystem.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChurchSystem.Web.Controllers
+{
+    public class FieldsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FieldsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fields.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Field @field = await _context.Fields
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            return View(@field);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Field field)
+        {
+            if (ModelState.IsValid)
+            {
+                try 
+                {
+                    _context.Add(field);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(field);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Field field = await _context.Fields.FindAsync(id);
+            if (field == null)
+            {
+                return NotFound();
+            }
+            return View(field);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Field field)
+        {
+            if (id != field.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(field);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(field);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Field @field = await _context.Fields
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fields.Remove(@field);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool FieldExists(int id)
+        {
+            return _context.Fields.Any(e => e.Id == id);
+        }
+    }
+}
